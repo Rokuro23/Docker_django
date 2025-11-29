17833d3f7ba08964a48d1da6fbc9652bd31a5446
Rokuro23 <efra.pasarin@gmail.com>
2025-11-29 01:20:44 -0300

    adjunta comentario

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 cd /app
 
+#use pip install si quieres usar otras librerias o ir al repositorio de django para actualizar el requirements.txt
+
 echo "Aplicando migraciones..."
 python manage.py migrate
 
